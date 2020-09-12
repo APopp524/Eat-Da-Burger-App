73483359630f0e22dc1ac67737c2942d151b98bc
Andrew Popp <apopp524@gmail.com>
2020-09-11 22:53:17 -0500

    bug fixes

--- config/connection.js
+++ config/connection.js
@@ -8,9 +8,7 @@ if (process.env.JAWSDB_URL) {
 	// DB is JawsDB on Heroku
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-	// DB is local on localhost
 	connection = mysql.createConnection({
-		port: 3306,
 		host: 'localhost',
 		user: 'root',
 		password: 'password',
